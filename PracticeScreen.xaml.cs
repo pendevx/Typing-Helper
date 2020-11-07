aaeea35a7c32e84e200a92e1e9a02adfd4a7f6fa
Pendevx <guangheng1234@gmail.com>
2020-11-08 01:01:43 +1300

    Add project files.

---
+++ PracticeScreen.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Typing_Helper
+{
+	/// <summary>
+	/// Interaction logic for PracticeScreen.xaml
+	/// </summary>
+	public partial class PracticeScreen : Page
+	{
+		public PracticeScreen()
+		{
+			InitializeComponent();
+
+			double lineHeight = 0.5;
+
+			displayedText.SetValue(Block.LineHeightProperty, lineHeight);
+			inputText.SetValue(Block.LineHeightProperty, lineHeight);
+		}
+	}
+}
